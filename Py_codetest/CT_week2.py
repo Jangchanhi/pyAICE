3681a10d4635a4e1ccebcae4f792d208cb58f807
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-03-28 03:27:49 +0900

    코테 스터디 그리디 파트 A02 문제

--- Py_codetest/CT_week2.py
+++ Py_codetest/CT_week2.py
@@ -21,20 +21,48 @@
 #
 # print("총 그룹의 수는 ?")
 # print(result)
+#
+# print(" A 02 곱하기 혹은 더하기 문제")
+#
+# data = input()
+#
+# result = int(data[0])
+#
+# for i in range(1, len(data)):
+#     num = int(data[i])
+#     if num <= 1 or result <= 1:
+#         result += num
+#     else:
+#         result *= num
+# print(result)
 
-print(" A 02 곱하기 혹은 더하기 문제")
+print("A03 문자열 뒤집기")
 
 data = input()
 
-result = int(data[0])
+count0 = 0
+count1 = 1
 
-for i in range(1, len(data)):
+if data[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
 
-    num = int(data[i])
-    if num <= 1 or result  <= 1:
-        result += num
-    else:
-        result *= num
-print(result)
+for i in range(len(data)-1):
+    if data[i] != data[i+1]:
+        if data[i+1] == '1':
+            count0 += 1
+        else:
+            count1 +=1
+print(min(count0, count1))
+
+# A04 문제 만들 수 없는 금액
+
+
+
+
+
+
+
 
 
