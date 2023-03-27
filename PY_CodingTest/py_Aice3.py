51bbce2193225db0ba3db8aedbe6c42723f85edc
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-03-28 03:34:14 +0900

    AICE 3주차 concat 연습

--- PY_CodingTest/py_Aice3.py
+++ PY_CodingTest/py_Aice3.py
@@ -13,3 +13,20 @@ print(data.pivot)
 data.pivot_table(index = ['cust_id', 'grade'], columns = 'prod_cd', values='pch_amt')
 
 data.pivot_table(index= 'cust_id', columns=['grade', 'prod_cd'], values='pch_amt')
+
+
+from IPython.display import Image
+df1 = pd.DataFrame({'key1' : [0,1,2,3,4], 'value1' : ['a', 'b', 'c', 'd', 'e']}, index=[0,1,2,3,4])
+df2 = pd.DataFrame({'key1' : [3,4,5,6,7], 'value1' : ['c', 'd', 'e', 'f', 'g']}, index=[3,4,5,6,7])
+
+print("<df1>")
+print(df1)
+
+print("<df2>")
+print(df2)
+
+print("<concat 연습, index=False>")
+print(pd.concat([df1, df2], ignore_index=False))
+print("concat 연습, index=True")
+print(pd.concat([df1, df2], ignore_index=True))
+
