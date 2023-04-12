5bb7ce8b23609d95ba9f6bf05ce569108a4b7f4a
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-12 12:55:15 +0900

    이것이 코딩 테스트다 part.구현 문제

--- Py_codetest/CT_implement_Q.py
+++ Py_codetest/CT_implement_Q.py
@@ -1,22 +1,70 @@
 # 07 럭키 스트레이트
+#
+# print("입력하세요")
+# n = input()
+# n_lengh = len(n)
+# summary = 0
+#
+# # 왼쪽 부분의 자릿수 합 더하기
+# for i in range(n_lengh//2):
+#     summary += int(n[i])
+#
+# # 오른쪽 부분의 자릿수 합 더하기:
+# for i in range(n_lengh//2, n_lengh):
+#     summary -= int(n[i])
+#
+# if summary == 0:
+#     print('LUCKY')
+# else :
+#     print("READY")
 
-print("입력하세요")
-n = input()
-n_lengh = len(n)
-summary = 0
+# 다른 방법 2
+n_array = list(map(int, input()))
+a = 0 # 앞의 줄
+b = 0 # 뒤의 줄
 
-# 왼쪽 부분의 자릿수 합 더하기
-for i in range(n_lengh//2):
-    summary += int(n[i])
-    
-# 오른쪽 부분의 자릿수 합 더하기:
-for i in range(n_lengh//2, n_lengh):
-    summary -= int(n[i])
+for i in range(len(n_array)):
+    n = len(n_array) / 2
+    if i < n:
+        a += n_array[i]
+    else:
+        b += n_array[i]
+if a == b:
+    print("Lucky")
+else:
+    print("Ready")
 
-if summary == 0:
-    print('LUCKY')
-else :
-    print("READY")
+
+
+
+
+# 08 문자열 재정렬
+#
+# data = input()
+# result = []
+# value = 0
+#
+#
+# # 문자를 하나씩 학인
+# for x in data:
+#     if x.isalpha():
+#         result.append()
+#     else:
+#         value += int(x)
+#
+# result.sort()
+#
+# if value != 0 :
+#     result.append(str(value))
+#
+# print(''.join(result))
+#
+# # 09 문자열 압축
+#
+# def test(s):
+#     result = len(s)
+
+
 
 
 
