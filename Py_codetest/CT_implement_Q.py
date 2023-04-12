e121c0cfb91c8b360dad5a9370c4872a59ae1764
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-04-12 13:12:36 +0900

    이것이 코딩 테스트다 part.구현 문제 09 문자열 압축

--- Py_codetest/CT_implement_Q.py
+++ Py_codetest/CT_implement_Q.py
@@ -18,24 +18,21 @@
 # else :
 #     print("READY")
 
-# 다른 방법 2
-n_array = list(map(int, input()))
-a = 0 # 앞의 줄
-b = 0 # 뒤의 줄
-
-for i in range(len(n_array)):
-    n = len(n_array) / 2
-    if i < n:
-        a += n_array[i]
-    else:
-        b += n_array[i]
-if a == b:
-    print("Lucky")
-else:
-    print("Ready")
-
-
-
+# # 다른 방법 2
+# n_array = list(map(int, input()))
+# a = 0 # 앞의 줄
+# b = 0 # 뒤의 줄
+#
+# for i in range(len(n_array)):
+#     n = len(n_array) / 2
+#     if i < n:
+#         a += n_array[i]
+#     else:
+#         b += n_array[i]
+# if a == b:
+#     print("Lucky")
+# else:
+#     print("Ready")
 
 
 # 08 문자열 재정렬
@@ -60,9 +57,32 @@ else:
 # print(''.join(result))
 #
 # # 09 문자열 압축
-#
-# def test(s):
-#     result = len(s)
+
+def solution(s):
+    result = []
+    if len(s) ==1:
+        return 1
+
+    for i in range(1, len(s+1)):
+        b = ''
+        count = 1
+        tmp= s[:i]
+
+        for j in range(i, len(s)+i, i):
+
+            if tmp==s[j:i+1]:
+                count+=1
+            else:
+                if count!=1:
+                    b = b+str(count)+tmp
+                else:
+                    b = b + tmp
+
+                tmp = s[j: j+1]
+                count = 1
+        result.append(len(b))
+
+    return min(result)
 
 
 
