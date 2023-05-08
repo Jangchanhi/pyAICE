3e88d1c1494e29ec3afbab8efc7fcd24fc803617
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-08 23:57:04 +0900

    이것이 코딩 테스트다 part.DFS/BFS 실전 문제 : 음료수 얼려먹기 기본 정답

--- Py_codetest/CT_DFS_BFS.py
+++ Py_codetest/CT_DFS_BFS.py
@@ -126,7 +126,7 @@ for i in range(n):
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
 def dfs_ice(x,y):
     # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y > m:
+    if x <= -1 or x >= n or y <= -1 or y >= n:
         return False
     # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
@@ -134,8 +134,8 @@ def dfs_ice(x,y):
         graph[x][y] = 1
         # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
         dfs_ice(x - 1, y)
-        dfs_ice(x, y - 1)
         dfs_ice(x + 1, y)
+        dfs_ice(x, y - 1)
         dfs_ice(x, y + 1)
         return True
     return False
@@ -148,7 +148,6 @@ for i in range(n):
         if dfs_ice(i, j) == True:
             result += 1
 
-print("아이스크림의 개수는?")
 print(result)
 
 
