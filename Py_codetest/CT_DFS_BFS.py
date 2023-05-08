d0c94c1f1c0c357a3fb107b3213f69bb41676e56
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-08 23:52:46 +0900

    이것이 코딩 테스트다 part.DFS/BFS 실전 문제 : 음료수 얼려먹기 기본 정답

--- Py_codetest/CT_DFS_BFS.py
+++ Py_codetest/CT_DFS_BFS.py
@@ -56,14 +56,14 @@ from collections import deque
 # 5-8 DFS 예제 => 스택 자료구조에 기초한다 (실제는 스텍을 안써도 되며 탐색할 때 데이터가 N개일 경우 O(N)의 시간이 소요된다.
 # DFS 특징 : 재귀 함수를 이용할 때 간결하게 구현 가능하다, 최대한 멀리 있는 노드를 우선으로 탐색하는 방식
 # 
-# def dfs(graph, v, visited):
-#     # 현재노드를 방문 처리
-#     visited[v] = True
-#     print(v, end=' ')
-#     # 현재노드와 연결된 다른 노드를 재귀적으로 방문
-#     for i in graph[v]:
-#         if not visited[i]:
-#             dfs(graph, i, visited)
+def dfs(graph, v, visited):
+    # 현재노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 # # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 # graph = [
 #     [],
@@ -83,21 +83,21 @@ from collections import deque
 
 # BFS : 너비 우선 탐색 => 가까운 노드부터 탐색하는 알고리즘
 # # 5-9 BFS 예제
-# def bfs(graph, start, visited):
-#     # 큐 구현을 위해 deque 라이브러리 사용
-#     queue = deque([start])
-#     visited[start] = True
-#     # 큐가 빌 때까지 반복
-#     while queue:
-#         # 큐에서 하나의 원소를 뽑아 출력
-#         v = queue.popleft()
-#         print(v, end=' ')
-#         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 queue.append(i)
-#                 visited[i] = True
-# 
+def bfs(graph, start, visited):
+    # 큐 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
 # # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 # graph = [
 #     [],
@@ -124,9 +124,9 @@ for i in range(n):
     graph.append(list(map(int, input())))
 
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x,y):
-    # 주어진 범위를 벗어난느 경우에는 즉시 종료
-    if x <= -1 or x > n or y <= -1 or y > m:
+def dfs_ice(x,y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y > m:
         return False
     # 현재 노드를 아직 방문하지 않았다면
     if graph[x][y] == 0:
