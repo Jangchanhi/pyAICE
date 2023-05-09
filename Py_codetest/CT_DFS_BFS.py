c841fa24b7529aa1c2b8751dee0a8dc439ffc05f
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-10 01:20:46 +0900

    이것이 코딩 테스트다 DFS/BFS 실전 문제 : 미로탈출

--- Py_codetest/CT_DFS_BFS.py
+++ Py_codetest/CT_DFS_BFS.py
@@ -56,14 +56,14 @@ from collections import deque
 # 5-8 DFS 예제 => 스택 자료구조에 기초한다 (실제는 스텍을 안써도 되며 탐색할 때 데이터가 N개일 경우 O(N)의 시간이 소요된다.
 # DFS 특징 : 재귀 함수를 이용할 때 간결하게 구현 가능하다, 최대한 멀리 있는 노드를 우선으로 탐색하는 방식
 # 
-def dfs(graph, v, visited):
-    # 현재노드를 방문 처리
-    visited[v] = True
-    print(v, end=' ')
-    # 현재노드와 연결된 다른 노드를 재귀적으로 방문
-    for i in graph[v]:
-        if not visited[i]:
-            dfs_ice(graph, i)
+# def dfs(graph, v, visited):
+#     # 현재노드를 방문 처리
+#     visited[v] = True
+#     print(v, end=' ')
+#     # 현재노드와 연결된 다른 노드를 재귀적으로 방문
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs_ice(graph, i)
 # # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
 # graph = [
 #     [],
@@ -115,7 +115,7 @@ def bfs(graph, start, visited):
 # bfs(graph, 1, visited) # BFS 함수 호출
 
 # 5-10문제 : 음료수 얼려먹기
-# # N, M을 공백으로 구분해서 입력 받음
+# N, M을 공백으로 구분해서 입력 받음
 # n, m = map(int, input().split())
 #
 # # 2차원 리스트의 맴 정보 입력받기
@@ -125,21 +125,21 @@ def bfs(graph, start, visited):
 #
 # print(graph)
 # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs_ice(x,y):
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] == 0:
-        # 해당 노드 방문 처리
-        graph[x][y] = 1
-        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
-        dfs_ice(x - 1, y)
-        dfs_ice(x + 1, y)
-        dfs_ice(x, y - 1)
-        dfs_ice(x, y + 1)
-        return True
-    return False
+# def dfs_ice(x,y):
+#     # 주어진 범위를 벗어나는 경우에는 즉시 종료
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+#     # 현재 노드를 아직 방문하지 않았다면
+#     if graph[x][y] == 0:
+#         # 해당 노드 방문 처리
+#         graph[x][y] = 1
+#         # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
+#         dfs_ice(x - 1, y)
+#         dfs_ice(x + 1, y)
+#         dfs_ice(x, y - 1)
+#         dfs_ice(x, y + 1)
+#         return True
+#     return False
 
 # # 모든 노드(위치)에 대하여 음료수 채우기
 # result = 0
@@ -153,47 +153,77 @@ def dfs_ice(x,y):
 
 # 문제 5-11 미로탈출
 
-# BFS 소스코드 구현
-def bfs(x, y):
-    # 큐 구현을 위해 deque 라이브러리 사용
+# # BFS 소스코드 구현
+# def bfs(x, y):
+#     # 큐 구현을 위해 deque 라이브러리 사용
+#     queue = deque()
+#     queue.append((x, y))
+#
+#     while queue:
+#         x, y = queue.popleft()
+#         # 현재 위치에서 4가지 방향으로의 위치 확인
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#             # 미로 찾기 공간을 벗어난 경우 무시
+#             if nx < 0 or ny < 0 or nx >= n or ny >= m:
+#                 continue
+#             # 벽인 경우 무시
+#             if graph[nx][ny] == 0:
+#                 continue
+#             # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = graph[x][y] + 1
+#                 queue.append((nx, ny))
+#     # 가장 오른쪽 아래까지의 최단 거리 반환
+#     return graph[n - 1][m - 1]
+#
+#
+# from collections import deque
+#
+# # N, M을 공백을 기준으로 구분하여 입력 받기
+# n, m = map(int, input().split())
+# # 2차원 리스트의 맵 정보 입력 받기
+# graph = []
+#
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+# # 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+#
+# # BFS를 수행한 결과 출력
+# print(bfs(0, 0))
+
+# 5-11 방법 2
+
+n, m = map(int, input().split())
+
+ec_graph=[]
+for i in range(n):
+    ec_graph.append(list(map(int, input())))
+
+mapx = [-1,1,0,0]
+mapy = [0,0,-1,1]
+
+def excape_bfs(x,y):
     queue = deque()
-    queue.append((x, y))
+    queue.append((x,y))
 
     while queue:
         x, y = queue.popleft()
-        # 현재 위치에서 4가지 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+        for j in range(4):
+            rx = x + mapx[j]
+            ry = y + mapy[j]
+
+            if rx < 0 or rx > n - 1 or ry < 0 or ry > m - 1:
                 continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
+            if ec_graph[rx][ry] == 0:
                 continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
+            if ec_graph[rx][ry] == 1:
+                ec_graph[rx][ry] = ec_graph[n][m] + 1
+                queue.append((rx, ry))
+    return ec_graph[n-1][m-1]
 
-
-from collections import deque
-
-# N, M을 공백을 기준으로 구분하여 입력 받기
-n, m = map(int, input().split())
-# 2차원 리스트의 맵 정보 입력 받기
-graph = []
-
-for i in range(n):
-    graph.append(list(map(int, input())))
-# 이동할 네 가지 방향 정의 (상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# BFS를 수행한 결과 출력
-print(bfs(0, 0))
-
-
+print(excape_bfs(0,0))
 
