224341950b8e2b375e467df7c21920040243ccb3
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-16 01:05:11 +0900

    이것이 코딩 테스트다 DFS/BFS Q16

--- Py_codetest/Q_DFS_BFS.py
+++ Py_codetest/Q_DFS_BFS.py
@@ -37,7 +37,31 @@ if check == False:
     print(-1)
 
 
-# Q16
+# Q16 연구소
+n, m = map(int, input().split())
+data = [] # 초기 멥 리스트
+temp = [[0] * m for _ in range(n)]
+
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+# 4가지 이동 방향에 대한 리스트
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+result = 0
+
+# 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
+        if nx >= 0 and nx < n and ny >= 0 and ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
 
 
 
