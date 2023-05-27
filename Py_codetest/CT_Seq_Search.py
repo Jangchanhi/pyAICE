c45274e936dc4ad5b9705c6ab7a43bf720a0f5fb
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-27 23:56:51 +0900

    이코테 : 순차 탐색 7-1 순차 탐색 소스코드 구현

--- Py_codetest/CT_Seq_Search.py
+++ Py_codetest/CT_Seq_Search.py
@@ -24,7 +24,13 @@ def binary_search(array, target, start, end):
         return None
     mid = (start + end) // 2
     # 찾은 경우 중간점 인덱스 변환
-
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾는 값이 작을 때 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid -1)
+    else:
+        return binary_search(array, target, mid + 1, end)
 
 
 
