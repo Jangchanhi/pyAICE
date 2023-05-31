5cda0fa92e7f91cbc49788ba15c437ecdbd10e93
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-31 12:42:11 +0900

    이코테 : 이진 탐색 7-3 이진탐색 => 반복문으로 구현

--- Py_codetest/CT_Seq_Search.py
+++ Py_codetest/CT_Seq_Search.py
@@ -19,7 +19,7 @@ def sequential_search(n, target, array):
 # print(sequential_search(n, target, array))
 
 # 7-2 재귀 함수로 구현한 이진 탐색 소스코드
-def binary_search(array, target, start, end):
+def binary_search_J(array, target, start, end):
     if start > end:
         return None
     mid = (start + end) // 2
@@ -28,25 +28,25 @@ def binary_search(array, target, start, end):
         return mid
     # 중간점의 값보다 찾는 값이 작을 때 왼쪽 확인
     elif array[mid] > target:
-        return binary_search(array, target, start, mid -1)
+        return binary_search_J(array, target, start, mid - 1)
     else:
-        return binary_search(array, target, mid + 1, end)
+        return binary_search_J(array, target, mid + 1, end)
 
-
-n, target = list(map(int, input().split()))
-
-array = list(map(int, input().split()))
-
-result = binary_search(array, target, 0, n -1)
-if result == None:
-    print("원소가 존재하지 않습니다.")
-else:
-    print(result + 1)
+#
+# n, target = list(map(int, input().split()))
+#
+# array = list(map(int, input().split()))
+#
+# result = binary_search_J(array, target, 0, n - 1)
+# if result == None:
+#     print("원소가 존재하지 않습니다.")
+# else:
+#     print(result + 1)
 
 
 # 7-3 반복문으로 구현한 이진 탐색 소스코드
 # 이진 탐색 소스코드 구현(반복분)
-def binary_search(array, target, start, end):
+def binary_search_b(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
         # 찾은 경우 중간점 인덱스 반환
@@ -60,20 +60,47 @@ def binary_search(array, target, start, end):
             start = mid + 1
     return None
 
-# n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
-n, target = list(map(int, input().split()))
-# 전체 원소 입력받기
+# # n(원소의 개수)과 target(찾고자 하는 문자열)을 입력받기
+# n, target = list(map(int, input().split()))
+# # 전체 원소 입력받기
+# array = list(map(int, input().split()))
+#
+# # 이진 탐색 수행 결과 출력
+# result = binary_search_b(array, target, 0, n - 1)
+# if result == None:
+#     print("원소가 존재하지 않습니다.")
+# else:
+#     print(result+1)
+
+# 7-5 실전 문제 2 부품 찾기
+def binary_search_n(array, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        # 찾은 경우 중간점 인덱스 반환
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 공배긍로 구분하여 입력
 array = list(map(int, input().split()))
+array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M (손님이 확인 요청한 후 부품개수)입력
+m = int(input()) # 손님이 확인 요청한 전체 부품를 공백으로 구분하고 입력
+x = list(map(int, input().split()))
 
-# 이진 탐색 수행 결과 출력
-result = binary_search(array, target, 0, n-1)
-if result == None:
-    print("원소가 존재하지 않습니다.")
-else:
-    print(result+1)
-
-
-
+#  손님이 확인 요청한 부품 번호를 하나식 확인
+for i in x:
+    result = binary_search_n(array, i, 0, n-1)
+    if result != None:
+        print('yes', end=' ')
+    else :
+        print('no', end=' ')
 
 
 
