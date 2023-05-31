48d21169ff5adcc19f68b2eb4262b9e7a5b4c706
Coldy <51781198+Jangchanhi@users.noreply.github.com>
2023-05-31 12:43:04 +0900

    이코테 : 이진 탐색 7-5 실전문제 : 부품찾기

--- Py_codetest/CT_Seq_Search.py
+++ Py_codetest/CT_Seq_Search.py
@@ -85,22 +85,22 @@ def binary_search_n(array, target, start, end):
             start = mid + 1
     return None
 
-# N(가게의 부품 개수) 입력
-n = int(input())
-# 가게에 있는 전체 부품 번호를 공배긍로 구분하여 입력
-array = list(map(int, input().split()))
-array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
-# M (손님이 확인 요청한 후 부품개수)입력
-m = int(input()) # 손님이 확인 요청한 전체 부품를 공백으로 구분하고 입력
-x = list(map(int, input().split()))
-
-#  손님이 확인 요청한 부품 번호를 하나식 확인
-for i in x:
-    result = binary_search_n(array, i, 0, n-1)
-    if result != None:
-        print('yes', end=' ')
-    else :
-        print('no', end=' ')
+# # N(가게의 부품 개수) 입력
+# n = int(input())
+# # 가게에 있는 전체 부품 번호를 공배긍로 구분하여 입력
+# array = list(map(int, input().split()))
+# array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# # M (손님이 확인 요청한 후 부품개수)입력
+# m = int(input()) # 손님이 확인 요청한 전체 부품를 공백으로 구분하고 입력
+# x = list(map(int, input().split()))
+#
+# #  손님이 확인 요청한 부품 번호를 하나식 확인
+# for i in x:
+#     result = binary_search_n(array, i, 0, n-1)
+#     if result != None:
+#         print('yes', end=' ')
+#     else :
+#         print('no', end=' ')
 
 
 
